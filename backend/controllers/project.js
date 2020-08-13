97642b85150506790d1a82a275c551c14bf794bf
Miguel Angel <mimaca359@gmail.com>
2020-08-13 09:16:30 +0200

    Web con Angular terminada

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -1,9 +1,12 @@
 'use strict'
 
 var Project = require('../models/project');
+var fs = require('fs');
+var path = require('path');
 
 var controller = {
-    home: function(req, res){
+	
+	home: function(req, res){
 		return res.status(200).send({
 			message: 'Soy la home'
 		});
@@ -13,47 +16,47 @@ var controller = {
 		return res.status(200).send({
 			message: "Soy el metodo o accion test del controlador de project"
 		});
-    },
-    saveProject: function(req,res){
-        // Objeto project
-        var project = new Project();
-        
-        //Lo que nos devuelve el POST
-        var params = req.body;
+	},
 
-        // Propiedades = Valor
+	saveProject: function(req, res){
+		var project = new Project();
+
+		var params = req.body;
 		project.name = params.name;
 		project.description = params.description;
 		project.category = params.category;
 		project.year = params.year;
-        project.langs = params.langs;
+		project.langs = params.langs;
 		project.image = null;
 
-       
 		project.save((err, projectStored) => {
-			if(err) return res.status(500).send({message: 'Error al guardar.'});
+			if(err) return res.status(500).send({message: 'Error al guardar el documento.'});
 
-			if(!projectStored) return res.status(404).send({message: 'No se ha podido guardar.'});
+			if(!projectStored) return res.status(404).send({message: 'No se ha podido guardar el proyecto.'});
 
 			return res.status(200).send({project: projectStored});
 		});
-    },
-    getproject: function(req, res){
-        var projectId = req.params.id;
+	},
 
-        if(projectId == null )  return res.status(404).send({message: 'El proyecto no existe.'});
+	getProject: function(req, res){
+		var projectId = req.params.id;
 
-        Project.findById(projectId, (err, project) =>{
-            if(err) return res.status(500).send({message: 'Error al buscar en la BD.'});
+		if(projectId == null) return res.status(404).send({message: 'El proyecto no existe.'});
 
-            if(!project) return res.status(404).send({message: 'El proyecto no existe.'});
+		Project.findById(projectId, (err, project) => {
+
+			if(err) return res.status(500).send({message: 'Error al devolver los datos.'});
+
+			if(!project) return res.status(404).send({message: 'El proyecto no existe.'});
 
 			return res.status(200).send({
 				project
 			});
-        })
-    },
-    getProjects: function(req, res){
+
+		});
+	},
+
+	getProjects: function(req, res){
 
 		Project.find({}).sort('-year').exec((err, projects) => {
 
@@ -64,9 +67,9 @@ var controller = {
 			return res.status(200).send({projects});
 		});
 
-    },
+	},
 
-    updateProject: function(req, res){
+	updateProject: function(req, res){
 		var projectId = req.params.id;
 		var update = req.body;
 
@@ -80,8 +83,9 @@ var controller = {
 			});
 		});
 
-    },
-    deleteProject: function(req, res){
+	},
+
+	deleteProject: function(req, res){
 		var projectId = req.params.id;
 
 		Project.findByIdAndRemove(projectId, (err, projectRemoved) => {
@@ -93,8 +97,9 @@ var controller = {
 				project: projectRemoved
 			});
 		});
-    },
-    uploadImage: function(req, res){
+	},
+
+	uploadImage: function(req, res){
 		var projectId = req.params.id;
 		var fileName = 'Imagen no subida...';
 
@@ -146,8 +151,6 @@ var controller = {
 		});
 	}
 
-    
 };
-
 
 module.exports = controller;
